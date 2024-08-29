1a2b06ce341eade98a030d9521bc892a29311b08
Anuj Dhiman <anujsharma132942@gmail.com>
2024-08-29 20:56:05 +0530

    surefile integrated

--- src/test/java/BadBlood/BaseTest/Basetest.java
+++ src/test/java/BadBlood/BaseTest/Basetest.java
@@ -1,10 +1,22 @@
 package BadBlood.BaseTest;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFName;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -12,7 +24,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import BadBlood.POM.LandingPage;
 
 public class Basetest {
@@ -64,5 +81,34 @@ public class Basetest {
 		data2.put("pass", "Anu@1234");
 		
 		return new Object[][] {{data1}, {data2}};
+	}
+	
+	public List<HashMap<String,String>> getJsonData() throws IOException {
+		File jsonData = new File(System.getProperty("user.dir")+"\\src\\main\\java\\BadBlood\\Utils\\data.json");
+		String cred = FileUtils.readFileToString(jsonData, StandardCharsets.UTF_8);
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> credList = mapper.readValue(cred, new TypeReference<List<HashMap<String, String>>>() {
+		});
+		
+		return credList;
+	}
+	
+	public Object[][] getExcelData() throws IOException {
+		FileInputStream excelFile = new FileInputStream(new File("E:\\Realme 7\\cred.xlsx"));
+		XSSFWorkbook wb = new XSSFWorkbook(excelFile);
+		XSSFSheet sheet = wb.getSheetAt(0);
+		int rowNum = sheet.getPhysicalNumberOfRows();
+		XSSFRow row = sheet.getRow(0);
+		int colNum = row.getLastCellNum();
+		Object[][] credentials = new Object[rowNum-1][colNum];
+		DataFormatter dataFormatter = new DataFormatter();
+		for(int i=0;i<rowNum-1; i++) {
+			XSSFRow r = sheet.getRow(i+1);
+			for(int j=0; j<colNum;j++) {
+				XSSFCell cel = r.getCell(j);
+				credentials[i][j] = dataFormatter.formatCellValue(cel);
+			}
+		}
+		return credentials;
 	}
 }
--- src/test/java/BadBlood/Tests/TestHomePage.java
+++ src/test/java/BadBlood/Tests/TestHomePage.java
@@ -1,6 +1,8 @@
 package BadBlood.Tests;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -10,19 +12,36 @@ import BadBlood.BaseTest.Basetest;
 import BadBlood.BaseTest.MyRetry;
 
 public class TestHomePage extends Basetest{
-	@Test(dataProvider = "dataprovider", groups = {"smoke"})
-	public void test1(HashMap<String, String> input) {
-		landingPage.login(input.get("email"), input.get("pass"));
+	@Test(dataProvider = "dataprovider")
+	public void test1(String cell1, String cell2, String cell3 ){
+//	public void test1(HashMap<String, String> input) {
+//		landingPage.login(input.get("email"), input.get("pass"));
+		System.out.println(cell1);
+		System.out.println(cell2);
+		System.out.println(cell3);
 	}
 	@Test(retryAnalyzer = MyRetry.class)
 	public void test2() throws InterruptedException {
 		landingPage.login("anu@example.com", "Anu@4321");
 		Assert.assertEquals(landingPage.getErrorMsg(), "Incorrect email o password.");
 	}
+	
 	@DataProvider
-	public Object[][] dataprovider() {
-		Object[][] cred = getHashMapData();
+	public Object[][] dataprovider() throws IOException {
+		Object[][] cred = getExcelData();
 		return cred;
 	}
+	
+//	@DataProvider
+//	public Object[][] dataprovider() {
+//		Object[][] cred = getHashMapData();
+//		return cred;
+//	}
+	
+//	@DataProvider
+//	public Object[][] dataprovider() throws IOException {
+//		List <HashMap<String, String>> inputData=getJsonData();
+//		return new Object[][] {{inputData.get(0)}, {inputData.get(1)}};
+//	}
 
 }
